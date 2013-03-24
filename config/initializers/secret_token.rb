4002f10590a0e573ef267cffd39c9b7e782bc22e
Brock Trappitt <brock.trappitt@gmail.com>
2013-03-24 09:26:19 +0800

    Created application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsCreateDifferences::Application.config.secret_key_base = 'f3d665269b69977898fff5d24bd31df018d7eb156cb01b04ce6b83ff410959f399e2ee15dc2f27e2b739bcdc07838bb0bcd3fe3974e236ff4ab3d66c68b2c37f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsCreateDifferences::Application.config.session_store :encrypted_cookie_store, key: '_rails_create_differences_session'
